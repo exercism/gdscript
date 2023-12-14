93ccde45702d63144ad660c380d3ded8da6c19b2
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-12-13 17:42:55 -0800

    Update test suite

--- exercises/practice/leap/leap_test.gd
+++ exercises/practice/leap/leap_test.gd
@@ -1,38 +1,26 @@
-const TEST_CASES = [
-	{"test_name": "year not divisible by 4 in common year",
-     "method_name": "leap",
-     "args": [2015],
-     "expected": false},
-	{"test_name": "year divisible by 2, not divisible by 4 in common year",
-     "method_name": "leap",
-     "args": [1970],
-     "expected": false},
-    {"test_name": "year divisible by 4, not divisible by 100 in leap year",
-     "method_name": "leap",
-     "args": [1996],
-     "expected": true},
-    {"test_name": "year divisible by 4 and 5 is still a leap year",
-     "method_name": "leap",
-     "args": [1960],
-     "expected": true},
-    {"test_name": "year divisible by 100, not divisible by 400 in common year",
-     "method_name": "leap",
-     "args": [2100],
-     "expected": false},
-    {"test_name": "year divisible by 100 but not by 3 is still not a leap year",
-     "method_name": "leap",
-     "args": [1900],
-     "expected": false},
-    {"test_name": "year divisible by 400 is leap year",
-     "method_name": "leap",
-     "args": [2000],
-     "expected": true},
-    {"test_name": "year divisible by 400 but not by 125 is still a leap year",
-     "method_name": "leap",
-     "args": [2400],
-     "expected": true},
-    {"test_name": "year divisible by 200, not divisible by 400 in common year",
-     "method_name": "leap",
-     "args": [1800],
-     "expected": false},
-]
+func test_year_not_divisible_by_4(solution_script):
+	return [false, solution_script.leap(2015)]
+
+func test_year_divisible_by_2_not_4(solution_script):
+     return [false, solution_script.leap(1970)]
+
+func test_year_divisible_by_4_not_100(solution_script):
+     return [true, solution_script.leap(1996)]
+
+func test_year_divisible_by_4_and_5(solution_script):
+     return [true, solution_script.leap(1960)]
+
+func test_year_divisible_by_100_not_400(solution_script):
+     return [false, solution_script.leap(2100)]
+
+func test_year_divisible_by_100_not_3(solution_script):
+     return [false, solution_script.leap(1900)]
+
+func test_year_divisible_by_400(solution_script):
+     return [true, solution_script.leap(2000)]
+
+func test_year_divisible_by_400_not_125(solution_script):
+     return [true, solution_script.leap(2400)]
+
+func test_year_divisible_by_200_not_400(solution_script):
+     return [false, solution_script.leap(1800)]
