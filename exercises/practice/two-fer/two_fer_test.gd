8559a3927b394303a3c897c1fbdc8b3c526d8d00
meatball <69751659+meatball133@users.noreply.github.com>
2024-02-20 09:55:41 +0000

    Update test name

--- exercises/practice/two-fer/two_fer_test.gd
+++ exercises/practice/two-fer/two_fer_test.gd
@@ -1,8 +1,8 @@
-func no_name_given(solution_script):
+func test_no_name_given(solution_script):
 	return [solution_script.two_fer(), "One for you, one for me."]
 
-func a_name_given(solution_script):
+func test_a_name_given(solution_script):
 	return [solution_script.two_fer("Alice"), "One for Alice, one for me."]
 
-func another_name_given(solution_script):
+func test_another_name_given(solution_script):
 	return [solution_script.two_fer("Bob"), "One for Bob, one for me."]
