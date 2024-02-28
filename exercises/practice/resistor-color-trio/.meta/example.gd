5c3931cefe3bde4edcfb2e8c5d85999c9dd44497
glaxxie <86179463+glaxxie@users.noreply.github.com>
2024-02-28 14:07:56 -0600

    Update example.gd
    Fix another spacing issue

--- exercises/practice/resistor-color-trio/.meta/example.gd
+++ exercises/practice/resistor-color-trio/.meta/example.gd
@@ -2,10 +2,10 @@ func color_code(colors):
 	var colors_code = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
 	var units = {
-        1e9: "gigaohms",
-        1e6: "megaohms",
-        1e3: "kiloohms"
-    }
+		1e9: "gigaohms",
+		1e6: "megaohms",
+		1e3: "kiloohms"
+		}
 
 	var base_value = colors_code.find(colors[0]) * 10 + colors_code.find(colors[1])
 	var magnitude = 10 ** colors_code.find(colors[2])
