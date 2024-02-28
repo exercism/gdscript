f2687a13380a849514c272cd315bcc9eb930b82b
glaxxie <86179463+glaxxie@users.noreply.github.com>
2024-02-28 14:12:05 -0600

    Add resistor-color-trio exercise (#53)
    * Add resistor-color-trio exercise
    
    * Fix spacing
    
    Fix spacing so methods are 2 empty lines apart.
    
    * Update space
    
    * Update example.gd
    
    Fix another spacing issue

---
+++ exercises/practice/resistor-color-trio/.meta/example.gd
@@ -0,0 +1,18 @@
+func color_code(colors):
+	var colors_code = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+	var units = {
+		1e9: "gigaohms",
+		1e6: "megaohms",
+		1e3: "kiloohms"
+		}
+
+	var base_value = colors_code.find(colors[0]) * 10 + colors_code.find(colors[1])
+	var magnitude = 10 ** colors_code.find(colors[2])
+	var total = base_value * magnitude
+
+	for key in units.keys():
+		if total >= key:
+			return str(total / key) + " " + units[key]
+
+	return str(total) + " ohms"
