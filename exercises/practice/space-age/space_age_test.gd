ea21f1d4d3ea0fbedad8d50dcbc2fe95fdee9b8f
Paweł Fertyk <pfertyk@users.noreply.github.com>
2024-06-03 21:16:19 +0200

    Improve error messages in space-age exercise (#68)
    [no important files changed]

--- exercises/practice/space-age/space_age_test.gd
+++ exercises/practice/space-age/space_age_test.gd
@@ -1,50 +1,42 @@
-func roughly_equal(n1, n2):
-	return abs(n1 - n2) <= 0.01
+func _round(value):
+	return snappedf(value, 0.01)
 
 
 func test_age_on_Earth(solution_script):
 	var age = solution_script.on_planet('Earth', 1000000000)
-	var expected = 31.69
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(31.69)]
 
 
 func test_age_on_Mercury(solution_script):
 	var age = solution_script.on_planet('Mercury', 2134835688)
-	var expected = 280.88
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(280.88)]
 
 
 func test_age_on_Venus(solution_script):
 	var age = solution_script.on_planet('Venus', 189839836)
-	var expected = 9.78
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(9.78)]
 
 
 func test_age_on_Mars(solution_script):
 	var age = solution_script.on_planet('Mars', 2129871239)
-	var expected = 35.88
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(35.88)]
 
 
 func test_age_on_Jupiter(solution_script):
 	var age = solution_script.on_planet('Jupiter', 901876382)
-	var expected = 2.41
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(2.41)]
 
 
 func test_age_on_Saturn(solution_script):
 	var age = solution_script.on_planet('Saturn', 2000000000)
-	var expected = 2.15
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(2.15)]
 
 
 func test_age_on_Uranus(solution_script):
 	var age = solution_script.on_planet('Uranus', 1210123456)
-	var expected = 0.46
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(0.46)]
 
 
 func test_age_on_Neptune(solution_script):
 	var age = solution_script.on_planet('Neptune', 1821023456)
-	var expected = 0.35
-	return [roughly_equal(age, expected), true]
+	return [_round(age), _round(0.35)]
