d7a1b05cae77fd0316749407b3475cd12f6f6373
Kevin Bloch <kev@codingthat.com>
2025-04-21 10:36:03 +0200

    Use `create_from_string` in example (compilation result isn't checked anyway)

--- exercises/practice/reverse-string/.meta/example.gd
+++ exercises/practice/reverse-string/.meta/example.gd
@@ -1,6 +1,5 @@
 func reverse(original):
-	var regex = RegEx.new()
-	regex.compile("\\X")
+	var regex = RegEx.create_from_string("\\X")
 	var clusters = regex.search_all(original).map(func(m): return m.get_string())
 	clusters.reverse()
 	return "".join(clusters)
