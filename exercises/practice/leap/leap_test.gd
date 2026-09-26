5a2f656645f0542613cab003e7645a52c9eece25
Isaac Good <github@isaacgood.com>
2026-09-25 21:56:11 -0700

    [leap] Switch to generated tests

--- exercises/practice/leap/leap_test.gd
+++ exercises/practice/leap/leap_test.gd
@@ -1,34 +1,52 @@
-func test_year_not_divisible_by_4(solution_script):
-	return [solution_script.leap(2015), false]
+func test_year_not_divisible_by_4_in_common_year(solution_script):
+	var got = solution_script.leap(2015)
+	var want = false
+	return [got, want]
 
 
-func test_year_divisible_by_2_not_4(solution_script):
-	return [solution_script.leap(1970), false]
+func test_year_divisible_by_2_not_divisible_by_4_in_common_year(solution_script):
+	var got = solution_script.leap(1970)
+	var want = false
+	return [got, want]
 
 
-func test_year_divisible_by_4_not_100(solution_script):
-	return [solution_script.leap(1996), true]
+func test_year_divisible_by_4_not_divisible_by_100_in_leap_year(solution_script):
+	var got = solution_script.leap(1996)
+	var want = true
+	return [got, want]
 
 
-func test_year_divisible_by_4_and_5(solution_script):
-	return [solution_script.leap(1960), true]
+func test_year_divisible_by_4_and_5_is_still_a_leap_year(solution_script):
+	var got = solution_script.leap(1960)
+	var want = true
+	return [got, want]
 
 
-func test_year_divisible_by_100_not_400(solution_script):
-	return [solution_script.leap(2100), false]
+func test_year_divisible_by_100_not_divisible_by_400_in_common_year(solution_script):
+	var got = solution_script.leap(2100)
+	var want = false
+	return [got, want]
 
 
-func test_year_divisible_by_100_not_3(solution_script):
-	return [solution_script.leap(1900), false]
+func test_year_divisible_by_100_but_not_by_3_is_still_not_a_leap_year(solution_script):
+	var got = solution_script.leap(1900)
+	var want = false
+	return [got, want]
 
 
-func test_year_divisible_by_400(solution_script):
-	return [solution_script.leap(2000), true]
+func test_year_divisible_by_400_is_leap_year(solution_script):
+	var got = solution_script.leap(2000)
+	var want = true
+	return [got, want]
 
 
-func test_year_divisible_by_400_not_125(solution_script):
-	return [solution_script.leap(2400), true]
+func test_year_divisible_by_400_but_not_by_125_is_still_a_leap_year(solution_script):
+	var got = solution_script.leap(2400)
+	var want = true
+	return [got, want]
 
 
-func test_year_divisible_by_200_not_400(solution_script):
-	return [ solution_script.leap(1800), false]
+func test_year_divisible_by_200_not_divisible_by_400_in_common_year(solution_script):
+	var got = solution_script.leap(1800)
+	var want = false
+	return [got, want]
