3e069b152d723504f89ea5473b8bac705c39f9b4
Isaac Good <github@isaacgood.com>
2026-09-25 21:45:20 -0700

    [eliuds-eggs] Switch to generated tests

--- exercises/practice/eliuds-eggs/eliuds_eggs_test.gd
+++ exercises/practice/eliuds-eggs/eliuds_eggs_test.gd
@@ -1,14 +1,22 @@
 func test_0_eggs(solution_script):
-	return [solution_script.egg_count(0), 0]
+	var got = solution_script.egg_count(0)
+	var want = 0
+	return [got, want]
 
 
 func test_1_egg(solution_script):
-	return [solution_script.egg_count(16), 1]
+	var got = solution_script.egg_count(16)
+	var want = 1
+	return [got, want]
 
 
 func test_4_eggs(solution_script):
-	return [solution_script.egg_count(89), 4]
+	var got = solution_script.egg_count(89)
+	var want = 4
+	return [got, want]
 
 
 func test_13_eggs(solution_script):
-	return [solution_script.egg_count(2000000000), 13]
+	var got = solution_script.egg_count(2000000000)
+	var want = 13
+	return [got, want]
