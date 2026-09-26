ddbb9ef64f4fa2e884a526366a99bbf4fa75ed23
Isaac Good <IsaacG@users.noreply.github.com>
2026-09-25 22:09:07 -0700

    [reverse-string] Switch to generated tests (#133)

--- exercises/practice/reverse-string/reverse_string_test.gd
+++ exercises/practice/reverse-string/reverse_string_test.gd
@@ -1,52 +1,52 @@
-func test_empty_originaling(solution_script):
-	var original = ""
-	var expected = ""
-	return [solution_script.reverse(original), expected]
+func test_an_empty_string(solution_script):
+	var want = ""
+	var got = solution_script.reverse("")
+	return [got, want]
 
 
 func test_a_word(solution_script):
-	var original = "robot"
-	var expected = "tobor"
-	return [solution_script.reverse(original), expected]
+	var want = "tobor"
+	var got = solution_script.reverse("robot")
+	return [got, want]
 
 
 func test_a_capitalized_word(solution_script):
-	var original = "Ramen"
-	var expected = "nemaR"
-	return [solution_script.reverse(original), expected]
+	var want = "nemaR"
+	var got = solution_script.reverse("Ramen")
+	return [got, want]
 
 
 func test_a_sentence_with_punctuation(solution_script):
-	var original = "I'm hungry!"
-	var expected = "!yrgnuh m'I"
-	return [solution_script.reverse(original), expected]
+	var want = "!yrgnuh m'I"
+	var got = solution_script.reverse("I'm hungry!")
+	return [got, want]
 
 
 func test_a_palindrome(solution_script):
-	var original = "racecar"
-	var expected = "racecar"
-	return [solution_script.reverse(original), expected]
+	var want = "racecar"
+	var got = solution_script.reverse("racecar")
+	return [got, want]
 
 
 func test_an_even_sized_word(solution_script):
-	var original = "drawer"
-	var expected = "reward"
-	return [solution_script.reverse(original), expected]
+	var want = "reward"
+	var got = solution_script.reverse("drawer")
+	return [got, want]
 
 
 func test_wide_characters(solution_script):
-	var original = "子猫"
-	var expected = "猫子"
-	return [solution_script.reverse(original), expected]
+	var want = "猫子"
+	var got = solution_script.reverse("子猫")
+	return [got, want]
 
 
 func test_grapheme_cluster_with_pre_combined_form(solution_script):
-	var original = "Würstchenstand"
-	var expected = "dnatsnehctsrüW"
-	return [solution_script.reverse(original), expected]
+	var want = "dnatsnehctsrüW"
+	var got = solution_script.reverse("Würstchenstand")
+	return [got, want]
 
 
 func test_grapheme_clusters(solution_script):
-	var original = "ผู้เขียนโปรแกรม"
-	var expected = "มรกแรปโนยขีเผู้"
-	return [solution_script.reverse(original), expected]
+	var want = "มรกแรปโนยขีเผู้"
+	var got = solution_script.reverse("ผู้เขียนโปรแกรม")
+	return [got, want]
